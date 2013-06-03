679ac755ed1b188cbacbcab1118d866a855ad0ec
Nicolas Vannier <nicolas.vannier93@gmail.com>
2013-06-04 01:34:06 +0200

    Added > Jump to authoritative move

--- Game/Characters/Unit.cs
+++ Game/Characters/Unit.cs
@@ -5,29 +5,9 @@ using System.Collections.Generic;
 public class Unit : MonoBehaviour
 {
     #region Public Members
-    public float speed;
-    public float jumpSpeed;
-	public float angularSpeed;
-    public Vector2 mouseSensitivity;
     #endregion
 
     #region Initialization
-    void OnNetworkInstantiate(NetworkMessageInfo info) {
-        //if (!networkView.isMine) {
-        //    enabled = false;
-        //    return;
-        //}
-
-        //FPSController fpsController = gameObject.AddComponent<FPSController>();
-        //fpsController.speed = speed;
-        //fpsController.jumpSpeed = jumpSpeed;
-        //fpsController.angularSpeed = angularSpeed;
-        //fpsController.mouseSensitivity = mouseSensitivity;
-
-        //Camera.main.transform.parent = this.transform;
-        //Camera.main.transform.localPosition = Vector3.zero;
-        //Camera.main.transform.localRotation = Quaternion.identity;
-	}
     #endregion
 
     #region Networking
--- Game/Engine/FPSControllerAuthoritative.cs
+++ Game/Engine/FPSControllerAuthoritative.cs
@@ -7,12 +7,13 @@ class FPSControllerAuthoritative : MonoBehaviour
     #region Private Members
     private float lastClientHInput = 0f;
     private float lastClientVInput = 0f;
+    private float lastClientJumpInput = 0f;
     private float serverCurrentHInput = 0f;
     private float serverCurrentVInput = 0f;
-    private bool grounded;
+    private float serverCurrentJumpInput = 0f;
 
-    //private Vector3 moveDirection = Vector3.zero;
-    //private CharacterController controller;
+    private bool grounded;
+    private Vector3 moveDirection = Vector3.zero;
     #endregion
 
     #region Public Members
@@ -24,7 +25,7 @@ class FPSControllerAuthoritative : MonoBehaviour
     public float maxVerticalAngle = 0.5f;
     public float gravity = 100.0f;
     public bool airControl = true;
-    //public Vector2 mouseSensitivity;
+    public Vector2 mouseSensitivity = new Vector2(1f, 100f);
     #endregion
 
     #region Initialization
@@ -37,62 +38,98 @@ class FPSControllerAuthoritative : MonoBehaviour
     #endregion
 
     #region Update
-    void Update() {
+    void FixedUpdate() {
+        CharacterController controller = GetComponent<CharacterController>();
+
         // Get the inputs only if we are the owner
-        if (owner != null && Network.player == owner) {
-            float HInput = Input.GetAxis("Horizontal");
-            float VInput = Input.GetAxis("Vertical");
+        if (Network.player == owner) {
+            float hInput = Input.GetAxis("Horizontal");
+            float vInput = Input.GetAxis("Vertical");
+            float jumpInput = Input.GetAxis("Jump");
 
             // If the input has changed since the last time
-            if (lastClientHInput != HInput || lastClientVInput != VInput) {
+            if (lastClientHInput != hInput || lastClientVInput != vInput || lastClientJumpInput != jumpInput) {
                 // We keep the new input as the last input
-                lastClientHInput = HInput;
-                lastClientVInput = VInput;
+                lastClientHInput = hInput;
+                lastClientVInput = vInput;
+                lastClientJumpInput = jumpInput;
 
                 if (Network.isServer) {
-                    SendMovementInput(HInput, VInput);
+                    SendMovementInput(hInput, vInput, jumpInput);
                 } else if (Network.isClient) {
-                    networkView.RPC("SendMovementInput", RPCMode.Server, HInput, VInput);
+                    networkView.RPC("SendMovementInput", RPCMode.Server, hInput, vInput, jumpInput);
                 }
             }
         }
 
+        // The movements are done on the server
         if (Network.isServer) {
-            Vector3 moveDirection = new Vector3(serverCurrentHInput, 0, serverCurrentVInput);
-            float speed = 5;
-            transform.Translate(speed * moveDirection * Time.deltaTime);
-            Debug.Log(transform.position);
+            if (grounded) {
+                moveDirection = new Vector3(serverCurrentHInput, 0, serverCurrentVInput);
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+                if (serverCurrentJumpInput != 0f) {
+                    moveDirection.y = jumpSpeed;
+                }
+            } else if (airControl) {
+                moveDirection.x = serverCurrentHInput * speed;
+                moveDirection.z = serverCurrentVInput * speed;
+                moveDirection = transform.TransformDirection(moveDirection);
+            }
 
-            //Vector3 moveDirection = new Vector3(serverCurrentHInput, 0, serverCurrentVInput);
-            //float speed = 5;
-            //transform.Translate(speed * moveDirection * Time.deltaTime);
-            //if (grounded) {
-            //    moveDirection = new Vector3(serverCurrentHInput, -0.75f, serverCurrentVInput);
-            //    moveDirection = transform.TransformDirection(moveDirection);
-            //    moveDirection *= speed;
-            //    if (Input.GetButton("Jump")) {
-            //        moveDirection.y = jumpSpeed;
-            //    }
+            // Apply gravity
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            // Move the controller
+            grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
+
+            //// Mouse movement
+            //float mHorizontal = Input.GetAxis("Mouse X");
+            //float mVertical = Input.GetAxis("Mouse Y");
+            //float sideTurn = angularSpeed * mHorizontal * mouseSensitivity.x * Time.deltaTime;
+            //float verticalTurn = -1 * mVertical * mouseSensitivity.y * Time.deltaTime;
+
+            //// Vertical rotation
+            //Camera.main.transform.Rotate(verticalTurn, 0f, 0f);
+
+            //// Capping the vertical rotation of the camera
+            //if (Camera.main.transform.localRotation.x < -maxVerticalAngle) {
+            //    Camera.main.transform.localRotation = new Quaternion(-maxVerticalAngle, Camera.main.transform.localRotation.y, Camera.main.transform.localRotation.z, Camera.main.transform.localRotation.w);
+            //} else if (Camera.main.transform.localRotation.x > maxVerticalAngle) {
+            //    Camera.main.transform.localRotation = new Quaternion(maxVerticalAngle, Camera.main.transform.localRotation.y, Camera.main.transform.localRotation.z, Camera.main.transform.localRotation.w);
             //}
 
-            //// Apply gravity
-            //moveDirection.y -= gravity * Time.deltaTime;
-
-            //// Move the controller
-            //grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;    // Translation
+            //// Side Rotation
+            //transform.RotateAroundLocal(Vector3.up, sideTurn);
         }
     }
     #endregion
 
     #region Networking
     void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
+        // Writing data (on the server)
         if (stream.isWriting) {
             Vector3 pos = transform.position;
+            Quaternion rot = transform.rotation;
+            Quaternion camRot = Camera.main.transform.localRotation;
+
             stream.Serialize(ref pos);
+            stream.Serialize(ref rot);
+            stream.Serialize(ref camRot);
+
+        // Receiving data (on the clients)
         } else {
             Vector3 posReceive = Vector3.zero;
+            Quaternion rotReceive = Quaternion.identity;
+            Quaternion camRotReceive = Quaternion.identity;
+
             stream.Serialize(ref posReceive);
+            stream.Serialize(ref rotReceive);
+            stream.Serialize(ref rotReceive);
+
             transform.position = posReceive;
+            transform.rotation = rotReceive;
+            Camera.main.transform.localRotation = camRotReceive;
         }
     }
     #endregion
@@ -111,9 +148,10 @@ class FPSControllerAuthoritative : MonoBehaviour
     }
 
     [RPC]
-    void SendMovementInput(float HInput, float VInput) {
-        serverCurrentHInput = HInput;
-        serverCurrentVInput = VInput;
+    void SendMovementInput(float hInput, float vInput, float jumpInput) {
+        serverCurrentHInput = hInput;
+        serverCurrentVInput = vInput;
+        serverCurrentJumpInput = jumpInput;
     }
     #endregion
 }
